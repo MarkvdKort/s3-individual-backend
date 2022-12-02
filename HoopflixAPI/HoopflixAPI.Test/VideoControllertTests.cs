e3071b1fd6cef695a96b3daad1b77f0018d9fb1c
Mark Kort <485091@student.fontys.nl>
2022-12-02 10:28:24 +0100

    Added first test

---
+++ HoopflixAPI/HoopflixAPI.Test/VideoControllertTests.cs
@@ -0,0 +1,32 @@
+﻿using HoopflixAPI.Controllers;
+using HoopflixAPI.Models;
+using HoopflixAPI.Test.Mocks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoopflixAPI.Test
+{
+    public class VideoControllertTests
+    {
+        [Fact]
+        public async void User_Can_Get_List_Of_Brands()
+        {
+            // Arrange
+            var videoRepositoryMock = MockIVideoRepository.GetMock();
+            var brandController = new VideoController(videoRepositoryMock.Object);
+
+            // Act
+            var response =  brandController.GetAllVideos();
+
+
+            // Assert
+            Assert.IsAssignableFrom<List<Video>>(response);
+            Assert.NotEmpty(response as List<Video>);
+        }
+    }
+}
--- HoopflixAPI/HoopflixAPI/Program.cs
+++ HoopflixAPI/HoopflixAPI/Program.cs
@@ -53,7 +53,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "http://localhost:5000"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "http://localhost:5000", "http://localhost:3001"));
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
