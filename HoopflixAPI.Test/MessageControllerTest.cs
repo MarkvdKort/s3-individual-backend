44e0f51e54f132656df56fb6d7fecfde9600b979
Mark Kort <485091@student.fontys.nl>
2022-12-12 11:22:07 +0100

    Added first version of message tests

---
+++ HoopflixAPI.Test/MessageControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using HoopflixAPI.Controllers;
+using HoopflixAPI.Test.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoopflixAPI.Test
+{
+    public class MessageControllerTest
+    {
+        [Fact]
+        public async void GetMessagesFromCertainChat_Returns_Messages_When_Chat_Exists()
+        {
+            // Arrange
+            var messageRepositoryMock = MockIMessageRepository.GetMock();
+            var messageController = new MessageController(messageRepositoryMock.Object);
+
+            // Act
+            var request = messageController.GetMessagesFromCertainChat(1);
+
+            // Assert
+            Assert.NotNull(request);
+        }
+    }
+}
---
+++ HoopflixAPI.Test/Mocks/MockIMessageRepository.cs
@@ -0,0 +1,69 @@
+﻿using HoopflixAPI.Models;
+using HoopflixAPI.Repositories;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoopflixAPI.Test.Mocks
+{
+    public class MockIMessageRepository
+    {
+        public static Mock<IMessageRepository> GetMock()
+        {
+            var mock = new Mock<IMessageRepository>();
+            List<VideoMessage> videoMessages = new List<VideoMessage>()
+            {
+                new VideoMessage()
+                {
+                    Message =
+                    {
+                        ID = 1,
+                        ChatID = 1,
+                        UserID = 1,
+                        MessageContent = "1",
+                        Time = DateTime.Now.ToString(),
+                        New = 1, 
+                        Type="Video"
+
+                    },
+                    Video =
+                    {
+                        ID = 1,
+                        Name = "Test1",
+                        Description = "description",
+                        Era = "2020",
+                        Paths = "path",
+                        Play = "play",
+                        Player = "player",
+                        Team = "team",
+                        Thumbnail = "thumbnail"
+                    }
+                    
+
+
+                },
+            };
+            List<Message> Messages = new List<Message>()
+            {
+                new Message()
+                {
+                        ID = 1,
+                        ChatID = 1,
+                        UserID = 1,
+                        MessageContent = "1",
+                        Time = DateTime.Now.ToString(),
+                        New = 1,
+                        Type="Video"
+                },
+            };
+
+            mock.Setup(m => m.GetMessagesFromCertainChat(It.IsAny<int>()))
+                .Returns((int id) => videoMessages.Where(o => o.Message.ChatID == id).ToList());
+
+            return mock;
+        }
+    }
+}
